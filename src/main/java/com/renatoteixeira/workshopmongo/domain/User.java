67608bedc203719d120d3ec4eaf9533e54fc4afb
Renato Teixeira <renatous93@gmail.com>
2024-06-03 12:37:29 +0100

    Conexao ao MongoDB com repository e service

--- src/main/java/com/renatoteixeira/workshopmongo/domain/User.java
+++ src/main/java/com/renatoteixeira/workshopmongo/domain/User.java
@@ -2,8 +2,13 @@ package com.renatoteixeira.workshopmongo.domain;
 
 import java.util.Objects;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
 public class User {
 
+	@Id
 	private String id;
 	private String name;
 	private String email;
---
+++ src/main/java/com/renatoteixeira/workshopmongo/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.renatoteixeira.workshopmongo.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.renatoteixeira.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
