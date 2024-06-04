3e97e4b959ee50e5fd090f1eadcd967bf524350e
Renato Teixeira <renatous93@gmail.com>
2024-06-04 12:53:31 +0100

    Acrescentar comentarios aos posts

--- src/main/java/com/renatoteixeira/workshopmongo/domain/Post.java
+++ src/main/java/com/renatoteixeira/workshopmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.renatoteixeira.workshopmongo.domain;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.renatoteixeira.workshopmongo.dto.AuthorDTO;
+import com.renatoteixeira.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post {
@@ -16,8 +19,9 @@ public class Post {
 	private Date date;
 	private String title;
 	private String body;
-	
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 		
@@ -69,6 +73,14 @@ public class Post {
 
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
 	}
 
 	@Override
