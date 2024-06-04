2b18ab64f9a8f3fb6410db072842bd36ccf2bae6
Renato Teixeira <renatous93@gmail.com>
2024-06-04 12:29:41 +0100

    Projecao dos dados do autor com DTO

--- src/main/java/com/renatoteixeira/workshopmongo/domain/Post.java
+++ src/main/java/com/renatoteixeira/workshopmongo/domain/Post.java
@@ -6,6 +6,8 @@ import java.util.Objects;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import com.renatoteixeira.workshopmongo.dto.AuthorDTO;
+
 @Document
 public class Post {
 
@@ -15,13 +17,13 @@ public class Post {
 	private String title;
 	private String body;
 	
-	private User author;
+	private AuthorDTO author;
 	
 	public Post() {
 		
 	}
 
-	public Post(String id, Date date, String title, String body, User author) {
+	public Post(String id, Date date, String title, String body, AuthorDTO author) {
 		this.id = id;
 		this.date = date;
 		this.title = title;
@@ -61,11 +63,11 @@ public class Post {
 		this.body = body;
 	}
 	
-	public User getAuthor() {
+	public AuthorDTO getAuthor() {
 		return author;
 	}
 
-	public void setAuthor(User author) {
+	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
 
